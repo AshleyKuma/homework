627baa952a94a8501c5e6e4a2841a70f778ac700
ashley.wang <ashley.wang@epictech.com.tw>
2023-09-05 16:26:58 +0800

    style: update icon for `favorite` icon

--- lib/tab/tabbar_item.dart
+++ lib/tab/tabbar_item.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 enum TabBarItem {
   industry(title: "產業", icon: Icons.business),
-  favorite(title: "追蹤", icon: Icons.favorite);
+  favorite(title: "追蹤", icon: Icons.star);
 
   const TabBarItem({
     required this.title,
